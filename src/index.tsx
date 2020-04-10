0518ac52edac9d3df6bf310c1d34360daacbc5de
Andrew DaRe <andrew@duh.re>
2020-04-09 22:01:26 -0700

    Added basic foundation components for style

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import "./styles/style.scss";
+
+function App() {
+  return (
+    <h1>Hello</h1>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("app"))
