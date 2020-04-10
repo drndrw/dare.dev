c218f4efb28cefd0ef1181eb69f1bcc8e2093286
Andrew DaRe <andrew@duh.re>
2020-04-09 20:35:38 -0700

    Added Webpack, Babel and Typescript configs

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var devConfig = {
+  mode: "development",
+  watch: true,
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      { test: /\.scss$/, use: [ "style-loader", "css-loader", "sass-loader" ] },
+      { test: /\.tsx?$/, loader: "babel-loader" },
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  }
+}
+
+var prodConfig = {
+  mode: "production",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      { test: /\.scss$/, use: [ "style-loader", "css-loader", "sass-loader" ] },
+      { test: /\.tsx?$/, loader: "babel-loader" },
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
+  }
+}
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    return devConfig
+  } else  {
+    return prodConfig
+  }
+};
