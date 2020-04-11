13a2623973c38ef582d4146c049608b90bb231da
Andrew DaRe <andrew@duh.re>
2020-04-11 13:05:48 -0700

    Added missing ')'

--- src/components/hamburger.tsx
+++ src/components/hamburger.tsx
@@ -6,7 +6,7 @@ export default function Hamburger(props: any) {
   const dispatch = useDispatch();
 
   return (
-    <div className={"hamburger" + (props.navState ? ' open' : '')} onClick={() => props.navState ? dispatch(toggleNav(false)): dispatch(toggleNav(true)}>
+    <div className={"hamburger" + (props.navState ? ' open' : '')} onClick={() => props.navState ? dispatch(toggleNav(false)): dispatch(toggleNav(true))}>
       <div className="hamburger-line top"></div>
       <div className="hamburger-line bottom"></div>
     </div>
