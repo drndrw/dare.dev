58dadc75a672df91170895616589efa6f233a692
Andrew DaRe <andrew@duh.re>
2020-04-11 21:26:59 -0700

    Added basic nav and menu overlay behavior

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import Header from "./components/header";
+import Menu from "./components/menu";
 
 import "./styles/style.scss";
 import { reduxStore } from './store';
@@ -13,6 +14,7 @@ function App() {
   return (
     <>
       <Header navState={navState} />
+      <Menu navState={navState} />
       <div className="wrapper">
         <p>Lorem ipsum dolor sit <a href="/">amet</a>, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>Lorem ipsum dolor sit <a href="/">amet</a>, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
