80d305f820a1ec54b19d348e7992a9a6c417add8
Andrew DaRe <andrew@duh.re>
2020-04-19 14:45:31 -0700

    Added footer content and style for mobile and desktop

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,26 @@
 import * as React from "react";
 import { Link } from 'react-router-dom';
 
+import GitHub from './svg/github';
+import LinkedIn from './svg/linkedin';
+
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="container flex">
         <div className="left">
           <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </div>
+        <div className="left">
+          <Link to="/resume">Resume</Link>
+          <Link to="/contact">Contact</Link>
         </div>
         <div className="right">
+          <div className="logo-bar">
+            <a target="_blank" href="https://www.linkedin.com/in/andrewdare/"><LinkedIn first /></a>
+            <a target="_blank" href="https://github.com/drndrw"><GitHub /></a>
+          </div>
           <span>&copy;2020 Andrew DaRe</span>
         </div>
       </div>
